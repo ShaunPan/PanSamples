78aa1167db895a0950dad6c3eacc788f1fa15cf0
Xiaopan <xiaopanzhu0@gmail.com>
2016-05-24 18:02:46 +0800

    FragmentSample

---
+++ FragmentSample/src/main/java/com/pan/fragmentsample/TestFragment.java
@@ -0,0 +1,29 @@
+package com.pan.fragmentsample;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Author:Pan
+ * Time:2016/5/24 17:23
+ * Description:
+ */
+public class TestFragment extends Fragment {
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
