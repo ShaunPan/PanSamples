a00a726274ac3b8f3c70c4a6cfcb4e33de6b0810
ShaunPan <zhuxiaopan@163.com>
2016-05-25 17:32:01 +0800

    FragmentSample

--- FragmentSample/src/main/java/com/pan/fragmentsample/MainActivity.java
+++ FragmentSample/src/main/java/com/pan/fragmentsample/MainActivity.java
@@ -1,18 +1,57 @@
 package com.pan.fragmentsample;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "LifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "Activity onCreate");
         setContentView(R.layout.activity_main);
-
 
 
     }
 
+    public void addFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_container,fragment,TAG)
+                .addToBackStack(null)
+                .commit();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Activity onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Activity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Activity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Activity onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Activity onDestroy");
+    }
 }
--- FragmentSample/src/main/java/com/pan/fragmentsample/TestFragment.java
+++ FragmentSample/src/main/java/com/pan/fragmentsample/TestFragment.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Author:Pan
@@ -12,18 +16,78 @@ import android.support.v4.app.Fragment;
  */
 public class TestFragment extends Fragment {
 
+    public static final String TAG = "LifeCycle";
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        Log.i(TAG, "Fragment onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Fragment onCreate");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "Fragment onCreateView");
+        return inflater.inflate(R.layout.fragment_test, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.i(TAG, "Fragment onActivityCreated");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.i(TAG, "Fragment onViewStateRestored");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "Fragment onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "Fragment onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "Fragment onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "Fragment onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "Fragment onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Fragment onDestroy");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+        Log.i(TAG, "Fragment onDetach");
     }
 }
